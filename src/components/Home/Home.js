4826166339b90673964a843eff0b073d56fb186f
Shourov98hassan <kamruzzamanshourov98@gmail.com>
2023-01-02 22:52:04 +0600

    add reset button and its functionality

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,11 +13,17 @@ const Home = () => {
         setCount(counts);
     }
 
+    const handleReset=()=>{
+        const counts = count*0;
+        setCount(counts)
+    }
+
     return (
         <div>
            <h1>{count}</h1>
            <button onClick={handleIncrease} disabled={count>4?true:false}>Increase</button>
            <button onClick={handleDecrease} disabled={count<1?true:false}>Decrease</button>
+           <button onClick={handleReset}>Reset</button>
         </div>
     );
 };
