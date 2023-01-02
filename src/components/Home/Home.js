1ac19430baf9815d165347df4e973aab88aca3a9
Shourov98hassan <kamruzzamanshourov98@gmail.com>
2023-01-02 22:44:30 +0600

    adding onclick event

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
+    const [count,setCount] = useState(0);
+
+    const handleIncrease=()=>{
+        const counts = count+1;
+        setCount(counts);
+    }
+
+    const handleDecrease=()=>{
+        const counts = count-1;
+        setCount(counts);
+    }
+
     return (
         <div>
-            <h1>this is homepage</h1>
+           <h1>{count}</h1>
+           <button onClick={handleIncrease}>Increase</button>
+           <button onClick={handleDecrease}>Decrease</button>
         </div>
     );
 };
